77054fc9412fe91460ecb649f2244c02af524dbb
BelLwaLkeR <secreter_d@yahoo.co.jp>
2016-07-30 23:59:24 +0900

    第二章まで完了。Playerを操作できるようにした。

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+    public float speed = 5;
+
+	// Update is called once per frame
+	void Update () {
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        Vector2 direction = input.normalized;
+
+        GetComponent<Rigidbody2D>().velocity = direction * speed;
+
+	}
+}
