d334a961ed502e0e022e4e59d595cc5d8643118c
BelLwaLkeR <secreter_d@yahoo.co.jp>
2016-07-31 09:39:51 +0900

    第三章まで完了。弾を発射するようにした。

---
+++ UnityTutorial/Assets/Scripts/Bullet.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+    public string targetTag;
+    public float speed = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody2D>().velocity = transform.up.normalized * speed;
+	}
+
+	public void OnCollisionEnter(Collider other) {
+		if (other.tag != targetTag) { return; }
+		Destroy(gameObject);
+	}
+}
---
+++ UnityTutorial/Assets/Scripts/Shot.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shot : MonoBehaviour {
+    public Bullet bullet;
+    public float interval;
+	// Use this for initialization
+	IEnumerator Start () {
+		while (true) {
+			Instantiate(bullet, transform.position, transform.rotation);
+			yield return new WaitForSeconds(interval);
+		}
+	}
+}
